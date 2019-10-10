15678fa1491783204f49f075a57607cd28b9df0e
gortron <gordon.lanza@gmail.com>
2019-10-10 15:19:00 +0100

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,27 @@
 class Dog
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = 'nervous'
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
+
+# Dog
+#   Instance methods
+#     initializes with a name and an Owner
+#     can change its owner
+#     can't change its name
+#     initializes with a nervous mood
+#     can change its mood
+#   Class methods
+#     knows all the dogs
